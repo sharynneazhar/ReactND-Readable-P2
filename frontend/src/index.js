1b6052f24fc7da8bff05ea3c611a87d7a728e867
Sharynne Azhar <sharynneazhar@gmail.com>
2017-11-28 15:51:24 -0600

    create store, setup up browser router, setup reducers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducers,
+  {}, // initial state
+  applyMiddleware(thunk)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
