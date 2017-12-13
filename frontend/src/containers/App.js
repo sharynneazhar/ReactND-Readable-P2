f6b948fc5f3ee7d4cef65b10dba9e083835bf12f
Sharynne Azhar <sharynneazhar@gmail.com>
2017-12-13 08:52:47 -0600

    Add BrowserRouter

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import { Switch, Route, withRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, withRouter } from 'react-router-dom';
 import PostContainer from '../containers/PostContainer';
+import PostDetailContainer from '../containers/PostDetailContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route exact path='/' component={PostContainer} />
-          <Route exact path='/:category' component={PostContainer} />
-        </Switch>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={PostContainer} />
+            <Route exact path='/:category' component={PostContainer} />
+            <Route exact path='/:category/:id' component={PostDetailContainer} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
